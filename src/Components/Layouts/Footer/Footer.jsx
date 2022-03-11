caf94eef8b8c5f900aabe419ba8bf93cecf13f25
SebasCepedaTobon <sebasct2204@gmail.com>
2022-03-11 11:34:50 -0500

    Se termina el footer

--- src/Components/Layouts/Footer/Footer.jsx
+++ src/Components/Layouts/Footer/Footer.jsx
@@ -1,18 +1,14 @@
 import React from 'react'
 import { Imagenes } from '../../UI/Imagenes/Imagenes'
-import facebook from '../../../assets/Imagenes/facebook.svg'
-import instagram from '../../../assets/Imagenes/instagram.svg'
-import email from '../../../assets/Imagenes/email.svg'
+import facebook from '../../../assets/Imagenes/facebook.png'
+import instagram from '../../../assets/Imagenes/whatsapp.png'
+import email from '../../../assets/Imagenes/gmail.png'
 import logo from '../../../assets/Imagenes/Booker1.png'
 
 
 export const Footer = () => {
   return (
-    <div className='footer' >
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
-        <path fill="#000000" fill-opacity="1" d="M0,160L0,64L360,64L360,192L720,192L720,192L1080,192L1080,64L1440,64L1440,320L1080,320L1080,320L720,320L720,320L360,320L360,320L0,320L0,320Z"></path>
-      </svg>
-      
+    <div className='footer'>      
       <div className="contenedor-elementos">
         <div className='contenedor-redes' >
           <Imagenes url={facebook} clase='facebook' />
