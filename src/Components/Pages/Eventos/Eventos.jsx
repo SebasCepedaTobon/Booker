de58f2d09b4c0fe81aafff11e2c273f681ad9eb0
SantiCortes2250 <santicortesrincon15@gmail.com>
2022-07-01 01:09:04 -0500

    nuevos cambios

--- src/Components/Pages/Eventos/Eventos.jsx
+++ src/Components/Pages/Eventos/Eventos.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import { Footer } from '../../Layouts/Footer/Footer'
 import { MainEventos } from '../../Layouts/MainEventos/MainEventos'
 import { Navegacion2 } from '../../UI/Navegacion/Navegacion2'
+import { Navegacion3 } from '../../UI/Navegacion/Navegacion3'
 
 export const Eventos = () => {
   return (
     <div>
+        <Navegacion3/>
         <Navegacion2/>
         <MainEventos/>
         <Footer/>
